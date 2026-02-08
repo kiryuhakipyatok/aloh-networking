56f7cd62716176c1cc34b13220084dcff943e007
kiryuhakipyatok <goodapple5207@gmail.com>
2026-02-08 22:44:21 +0300

    (logs fix)

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -27,7 +27,7 @@ func NewLogger(acfg config.App) *Logger {
 		if err != nil {
 			panic(fmt.Errorf("failed to open log file: %w", err))
 		}
-		writer = io.MultiWriter(logFile, os.Stdout)
+		writer = io.Writer(logFile)
 	}
 	switch env {
 	case local:
