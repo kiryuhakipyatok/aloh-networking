7e0e2d888db728bdc4709b90cdcabbb29d41d9e0
kiryuhakipyatok <goodapple5207@gmail.com>
2026-07-12 23:50:31 +0300

    event parses added

--- event.go
+++ event.go
@@ -1,6 +1,11 @@
 package alohnetwork
 
-import "github.com/kiryuhakipyatok/aloh-networking/internal/domain/services/networking"
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/kiryuhakipyatok/aloh-networking/internal/domain/services/networking"
+)
 
 const (
 	FULL_MUTE    = networking.FULL_MUTE
@@ -14,3 +19,95 @@ type (
 	Event       = networking.Event
 	GeneralData = networking.GeneralData
 )
+
+func MuteMicEvent(state bool) (Event, error) {
+	t := time.Now().UTC().Unix()
+	e := Event{
+		Typee:     MIC_MUTE,
+		Timestamp: t,
+	}
+	data, err := json.Marshal(state)
+	if err != nil {
+		return e, err
+	}
+	e.Data = data
+	return e, nil
+}
+
+func MuteFullEvent(state bool) (Event, error) {
+	t := time.Now().UTC().Unix()
+	e := Event{
+		Typee:     FULL_MUTE,
+		Timestamp: t,
+	}
+	data, err := json.Marshal(state)
+	if err != nil {
+		return e, err
+	}
+	e.Data = data
+	return e, nil
+}
+
+func HardDenoiseEvent(state bool) (Event, error) {
+	t := time.Now().UTC().Unix()
+	e := Event{
+		Typee:     HARD_DENOISE,
+		Timestamp: t,
+	}
+	data, err := json.Marshal(state)
+	if err != nil {
+		return e, err
+	}
+	e.Data = data
+	return e, nil
+}
+
+func SoftDenoiseEvent(state bool) (Event, error) {
+	t := time.Now().UTC().Unix()
+	e := Event{
+		Typee:     SOFT_DENOISE,
+		Timestamp: t,
+	}
+	data, err := json.Marshal(state)
+	if err != nil {
+		return e, err
+	}
+	e.Data = data
+	return e, nil
+}
+
+func GeneralEvent(fm, mm, hd, sd bool) (Event, error) {
+	t := time.Now().UTC().Unix()
+	e := Event{
+		Typee:     GENERAL,
+		Timestamp: t,
+	}
+	gd := GeneralData{
+		FullMute:    fm,
+		MicMute:     mm,
+		HardDenoise: hd,
+		SoftDenoise: sd,
+	}
+	data, err := json.Marshal(gd)
+	if err != nil {
+		return e, err
+	}
+	e.Data = data
+	return e, nil
+}
+
+func DataToState(data []byte) (bool, error) {
+	var state bool
+	if err := json.Unmarshal(data, &state); err != nil {
+		return state, err
+	}
+	return state, nil
+}
+
+func DataToGeneral(data []byte) (GeneralData, error) {
+	var general GeneralData
+	if err := json.Unmarshal(data, &general); err != nil {
+		return general, err
+	}
+	return general, nil
+}
