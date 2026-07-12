0cd298db041e2fa97249fba2b98563cf949f8541
kiryuhakipyatok <goodapple5207@gmail.com>
2026-07-12 19:26:26 +0300

    event denoises added

--- event.go
+++ event.go
@@ -3,8 +3,10 @@ package alohnetwork
 import "github.com/kiryuhakipyatok/aloh-networking/internal/domain/services/networking"
 
 const (
-	FULL_MUTE = networking.FULL_MUTE
-	MIC_MUTE  = networking.MIC_MUTE
+	FULL_MUTE    = networking.FULL_MUTE
+	MIC_MUTE     = networking.MIC_MUTE
+	HARD_DENOISE = networking.HARD_DENOISE
+	SOFT_DENOISE = networking.SOFT_DENOISE
 )
 
 type Event = networking.Event
--- internal/domain/services/networking/protocol.go
+++ internal/domain/services/networking/protocol.go
@@ -3,6 +3,8 @@ package networking
 const (
 	FULL_MUTE = iota
 	MIC_MUTE
+	HARD_DENOISE
+	SOFT_DENOISE
 )
 
 type Event struct {
