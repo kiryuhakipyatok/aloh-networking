5edcf1f99ffb55eee365f750094af27ef18c6937
kiryuhakipyatok <goodapple5207@gmail.com>
2026-09-17 22:26:30 +0300

    disconn fix

--- internal/domain/services/networking/helpers.go
+++ internal/domain/services/networking/helpers.go
@@ -99,12 +99,13 @@ func (ns *networkingServ) disconnectSession(session *models.Session, isLeaveInit
 		if session.EventStream != nil {
 			if err := session.EventStream.Close(); err != nil {
 				log.Error("failed to close event stream", logger.Err(err), userIdLog)
-			} else {
-				select {
-				case <-session.EventStream.Context().Done():
-				case <-ns.closeCtx.Done():
-				}
 			}
+			// } else {
+			// 	select {
+			// 	case <-session.EventStream.Context().Done():
+			// 	case <-ns.closeCtx.Done():
+			// 	}
+			// }
 		}
 
 		if session.Conn != nil {
