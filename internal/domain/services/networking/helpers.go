1d6975389bd484cf065b22d941acc6f8abfd28b7
kiryuhakipyatok <goodapple5207@gmail.com>
2026-09-22 17:16:41 +0300

    imm close agent

--- internal/domain/services/networking/helpers.go
+++ internal/domain/services/networking/helpers.go
@@ -116,11 +116,11 @@ func (ns *networkingServ) disconnectSession(session *models.Session, isLeaveInit
 			}
 		}
 		log.Info("session conn closed")
-		// if session.Agent != nil {
-		// 	if err := session.Agent.GracefulClose(); err != nil {
-		// 		log.Error("failed to close ice agent", logger.Err(err), userIdLog)
-		// 	}
-		// }
+		if session.Agent != nil {
+			if err := session.Agent.Close(); err != nil {
+				log.Error("failed to close ice agent", logger.Err(err), userIdLog)
+			}
+		}
 		pprof.Lookup("goroutine").WriteTo(os.Stderr, 1)
 		log.Info("agent closed")
 		close(session.VoiceChan)
